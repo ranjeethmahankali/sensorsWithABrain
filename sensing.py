a53c04dcc22ad5fe81d852040bd6b96900abe6ea
Ranjeeth Mahankali <ranjeethmahankali@gmail.com>
2017-05-26 15:10:13 -0700

    the frequency at which the data is read is now centrally controlled not separate for each sensor

--- sensing.py
+++ sensing.py
@@ -4,12 +4,19 @@ import random
 import numpy as np
 
 # initializing the arduino
-# B = Arduino('/dev/cu.usbserial-DN02PAGM')
-B = Arduino('COM3')
+# B = Arduino('COM3')
+B = Arduino('/dev/cu.usbserial-DN02PAGM')
+# this is the pin used to tell the brain that I am here - the truth value
+# to train against. so set this pin to high when u r here at the DMG and working
+IAM_HERE_PIN = B.get_pin('d:2:i')
+
 it = util.Iterator(B)
 it.start()
 # the dictionary of all the sensor instances initialized till now
 SENSOR = dict()
+
+READ_NUM = 20
+READ_INTERVAL = 0.025
 
 # returns a true if the pin num is not being used by any sensor
 def pinAvailable(num):
@@ -23,7 +30,7 @@ def pinAvailable(num):
 
 # this is a class for sensor
 class sensor:
-    def __init__(self, sensorName, pin_number, read_frequency = 5, read_duration = 1):
+    def __init__(self, sensorName, pin_number):
         # if the name or the pin_number are already used then aborting
         if (sensorName in SENSOR) or (not pinAvailable(pin_number)):
             raise ValueError("sensor name or pin number is repeated !")
@@ -31,10 +38,6 @@ class sensor:
         # creating a sensor instance
         self.name = sensorName
         self.pin = pin_number
-        # this is the read frequency of the sensor
-        self.frequency = read_frequency
-        # this is the duration for which the sensor will collect data in a single read
-        self.duration = read_duration
         # enabling reporting for this sensor
         B.analog[self.pin].enable_reporting()
         # adding the sensor to the dictionary of sensors
@@ -47,23 +50,15 @@ class sensor:
         return count
 
     # returns a single reading from sensor
-    def single_reading(self):
+    def reading(self):
         read = B.analog[self.pin].read()
         if not read is None:
             return read
         else:
             time.sleep(0.001)
-            return self.single_reading()
+            return self.eading()
     # reads the data from the sensor and returns it as an array
     # readings are taken for appropriate duration at appropriate frequency
-    def read(self):
-        count = self.read_length()
-        data = []
-        for _ in range(count):
-            data.append(self.single_reading())
-            time.sleep(interval)
-        
-        return data
 
 # this is a class for a motor - a motor nerve control for the NN
 class motor:
@@ -76,17 +71,16 @@ class motor:
 # returns all the sensed data
 def allSensors_read():
     data = []
-    for name in SENSOR:
-        data += SENSOR[name].read()
+    for i in range(READ_NUM):
+        for name in SENSOR:
+            data.append(SENSOR[name].reading())
+        time.sleep(READ_INTERVAL)
     
     return data
 
 # returns the total length of the data from all sensors
 def data_length_all():
-    num = 0
-    for name in SENSOR:
-        num += SENSOR[name].read_length()
-    return num
+     return len(SENSOR)*READ_NUM
 
 # this is where the main code begins - for testing
 # temp = sensor("temp", 0)
