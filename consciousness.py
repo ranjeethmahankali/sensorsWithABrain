0cfead8a3ecbe9786b700baf5144dc3cf2f8cfb8
Ranjeeth Mahankali <ranjeethmahankali@gmail.com>
2017-05-30 19:05:53 -0700

    now using a proper recurrent neural network

--- consciousness.py
+++ consciousness.py
@@ -1,5 +1,9 @@
 from theBrain import *
 from datetime import datetime
+import shutil
+
+SENSE_BUFFER = [np.zeros([TRUNC_BACKPROP_LENGTH, SENSOR_DATA_DIM]),
+                    np.zeros([TRUNC_BACKPROP_LENGTH, OUTPUT_DIM])]
 
 def truth_values(batch_size):
     # this is supposed to be high when I am here
@@ -8,43 +12,66 @@ def truth_values(batch_size):
     angle = 0
 
     return [[hour, detection, angle]]*batch_size
-    
 
-sensor_t, prev_step, truth_t = get_placeholders()
-output, prev_inf = run_model(sensor_t, prev_step)
-loss_t, optimStep = loss_optim(output, truth_t)
-h_accuracy, d_accuracy, acc_total = accuracy(output, truth_t)
+def create_buffer():
+    global SENSE_BUFFER
+    print("Creating sense buffer... please wait")
+    for i in range(TRUNC_BACKPROP_LENGTH):
+        updateBuffer()
+        time.sleep(0.5)
+    print("Buffer created.")
 
-denormalized = getInterpretation(output)
+def updateBuffer():
+    global SENSE_BUFFER
+    truth = np.array(truth_values(1))
+    sensed_data = np.array([sense.allSensors_read()])
+
+    SENSE_BUFFER[0] = np.concatenate([SENSE_BUFFER[0][1:,:], sensed_data], axis=0)
+    SENSE_BUFFER[1] = np.concatenate([SENSE_BUFFER[1][1:,:], truth], axis=0)
+
+
 
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
     # creating new summary writers and deleting old summary logs
-    # shutil.rmtree(log_dir, ignore_errors=True)
-    # train_writer, test_writer = getSummaryWriters(sess)
+    print("Preparing to log and serve the training summaries to tensorboard...")
+    shutil.rmtree(log_dir, ignore_errors=True)
+    train_writer, test_writer = getSummaryWriters(sess)
     
     # at the beginning previous time step data is set to zero 
-    prev = np.zeros([1,HIDDEN_DIM_3])
-
-    cycles = 12000
+    cycles = 200000
     testStep = 500
     saveStep = 1000
-    log_step = 100
+    log_step = 5
+    create_buffer()
+    _cur_state = np.zeros([SENSOR_DATA_DIM, STATE_SIZE], dtype=np.float32)
+    # print(type(_cur_state))
     for i in range(cycles):
         truth_val = truth_values(1)
-        den, out, prev, _ = sess.run([denormalized, output, prev_inf, optimStep], feed_dict={
-            sensor_t: [sense.allSensors_read()],
-            prev_step: prev,
-            truth_t: truth_val
-        })
-    
-        hour_pred = den[0].tolist()[0][0]
-        detection_pred = den[1].tolist()[0][0]
-        head_angle = den[2].tolist()[0][0]
+        sensed_data = [sense.allSensors_read()]
+        _, hour_pred, detection_pred, head_angle, _cur_state,loss_val,summary = sess.run([
+            optim_t,
+            hour_t,
+            detection_t,
+            motor_t,
+            cur_state,
+            loss_tensor,
+            merged
+            ], 
+            feed_dict={
+                input_series: SENSE_BUFFER[0],
+                truth_series: SENSE_BUFFER[1],
+                init_state: _cur_state
+            })
+        
+        updateBuffer()
+        
+        if i % log_step == 0: train_writer.add_summary(summary, i)
 
         # print(hour_pred, detection_pred, head_angle, truth_val[0][:2], out[0].tolist())
         print('Time: %s, Detection: %s, Angle: %s'%(hour_pred,detection_pred,head_angle))
+        print('Loss: %s'%loss_val)
 
         detection_LED.write(detection_pred)
-        head.write(head_angle)
+        head.write(min(max(0,head_angle),180))
         time.sleep(0.5)
--- theBrain.py
+++ theBrain.py
@@ -9,75 +9,95 @@ dist = sense.sensor("distance", 2)
 head = sense.motor("head", "d:9:s")
 detection_LED = sense.motor("detection_LED", "d:4:o")
 
+print("%s sensors registered"%len(sense.SENSOR))
+
 SENSOR_DATA_DIM = sense.data_length_all()
-HIDDEN_DIM_1 = 256
-HIDDEN_DIM_2 = 1024
-HIDDEN_DIM_3 = 512
 OUTPUT_DIM = 3
 # it outputs two numbers, one is the hour of the day normalized
 # the second number is the probability that I am in the room
 # the third number is the motor signal to the servo - normalized angle
 
+# this is the backprop limit into the past
+TRUNC_BACKPROP_LENGTH = 20
+# State size of the RNN
+STATE_SIZE = 5
+HIDDEN_DIM_STATE_1 = 10
+HIDDEN_DIM_STATE_2 = 20
+HIDDEN_DIM_1 = 512
+HIDDEN_DIM_2 = 768
 # Other constants from here
 NUM_HOURS = 24
 
 # these are all the variables in the model
 with tf.variable_scope('vars'):
-    wf1 = weightVariable([SENSOR_DATA_DIM, HIDDEN_DIM_1],'wf1')
+    # this is for the time step
+    wt = weightVariable([STATE_SIZE+1, HIDDEN_DIM_STATE_1],'wt')
+    bt = biasVariable([HIDDEN_DIM_STATE_1], 'bt')
+
+    ws1 = weightVariable([HIDDEN_DIM_STATE_1, HIDDEN_DIM_STATE_2],'ws1')
+    bs1 = biasVariable([HIDDEN_DIM_STATE_2], 'bs1')
+    
+    ws2 = weightVariable([HIDDEN_DIM_STATE_2, STATE_SIZE],'ws2')
+    bs2 = biasVariable([STATE_SIZE], 'bs2')
+
+    wf1 = weightVariable([SENSOR_DATA_DIM*STATE_SIZE, HIDDEN_DIM_1],'wf1')
     bf1 = biasVariable([HIDDEN_DIM_1], 'bf1')
     
     wf2 = weightVariable([HIDDEN_DIM_1, HIDDEN_DIM_2],'wf2')
     bf2 = biasVariable([HIDDEN_DIM_2], 'bf2')
+
+    wf3 = weightVariable([HIDDEN_DIM_2, OUTPUT_DIM],'wf3')
+    bf3 = weightVariable([OUTPUT_DIM],'bf3')
+
+# running the model in single time step
+def run_model(inp, cur_state):
+    inp_reshape = tf.reshape(inp, [SENSOR_DATA_DIM, 1])
+    state_concat = tf.concat(1, [inp_reshape, cur_state])
+    h_state1 = tf.nn.relu(tf.matmul(state_concat, wt) + bt)
     
-    wf3 = weightVariable([HIDDEN_DIM_2, HIDDEN_DIM_3],'wf3')
-    bf3 = biasVariable([HIDDEN_DIM_3], 'bf3')
+    h_state2 = tf.nn.relu(tf.matmul(h_state1, ws1)+bs1)
+    next_state = tf.nn.relu(tf.matmul(h_state2, ws2)+bs2)
 
-    wf4 = weightVariable([HIDDEN_DIM_3, OUTPUT_DIM],'wf4')
-    bf4 = biasVariable([OUTPUT_DIM], 'bf4')
+    h0 = tf.reshape(next_state, [1,SENSOR_DATA_DIM*STATE_SIZE])
+    h1 = tf.nn.relu(tf.matmul(h0, wf1)+bf1)
+    h2 = tf.nn.relu(tf.matmul(h1, wf2)+bf2)
 
-    # these are the weights and biases for the time step
-    wt = weightVariable([HIDDEN_DIM_3,HIDDEN_DIM_1], 'wt')
-    bt = biasVariable([HIDDEN_DIM_1], 'bt')
+    output = tf.nn.tanh(tf.matmul(h2, wf3)+bf3)
+    output = (output+1)/2
 
-# returns the placeholders for input
-def get_placeholders():
-    sensor_tensor = tf.placeholder(tf.float32, shape = [None, SENSOR_DATA_DIM])
-    prev_inf_tensor = tf.placeholder(tf.float32, shape=[None, HIDDEN_DIM_3])
-    truth_tensor = tf.placeholder(tf.float32, shape = [None, OUTPUT_DIM])
+    return [output, next_state]
 
-    return [sensor_tensor, prev_inf_tensor, truth_tensor]
-
-# the main model
-def run_model(sensor_data, prev_inference):
-    # sensor data is the data from the sensor
-    h1_val = tf.matmul(sensor_data, wf1) + bf1
-    h1_val += tf.matmul(prev_inference, wt) + bt
-    h1 = tf.nn.relu(h1_val)
-
-    h2 = tf.nn.relu(tf.matmul(h1, wf2) + bf2)
-    h3 = tf.nn.relu(tf.matmul(h2, wf3) + bf3)
-    output = tf.nn.tanh(tf.matmul(h3, wf4) + bf4)
-
-    norm = (output+1)/2
-    return [norm,h3]
 
 # currently implementing root mean squared error
-def loss_optim(prediction, truth):
+def loss(output_series, truth_series):
     split_axis = 1
-    norm_hour, norm_detection, norm_motor = tf.split(split_axis, OUTPUT_DIM, prediction)
-
     epsilon = 1e-6
-    hour = (norm_hour-epsilon)*NUM_HOURS
-    detection = norm_detection - epsilon
-    # in the truth data we ignore the angle
-    true_hour, true_detection, _ = getInterpretation(truth)
+    truths = tf.unpack(truth_series, axis=0)
+    total_loss = 0
+    motor_vals = []
+    for i in range(len(output_series)):
+        prediction = output_series[i]
+        truth = tf.reshape(truths[i],[1,3])
+        # print(truth.get_shape())
 
-    absDiff_h = tf.reduce_sum(tf.abs(hour - true_hour))
-    absDiff_d = tf.reduce_sum(tf.abs(detection - true_detection))
-    loss = absDiff_d + absDiff_h
-    optim = tf.train.AdamOptimizer(learning_rate).minimize(loss)
+        norm_hour, norm_detection, norm_motor = tf.split(split_axis, OUTPUT_DIM, prediction)
+        true_hour, true_detection, _ = tf.split(split_axis, OUTPUT_DIM, truth)
+        motor_vals.append(norm_motor)
 
-    return [loss, optim]
+        absDiff_h = tf.reduce_sum(tf.abs(norm_hour - true_hour))
+        absDiff_d = tf.reduce_sum(tf.abs(norm_detection - true_detection))
+        loss = absDiff_d + absDiff_h
+        total_loss += loss
+    
+    # rewarding the trait of looking around
+    motor_history = tf.concat(0,motor_vals)
+    mean, variance = tf.nn.moments(motor_history, axes=[0])
+    # print(variance.get_shape())
+
+    total_loss -= alpha * tf.reduce_mean(variance)
+
+    optim = tf.train.AdamOptimizer(learning_rate).minimize(total_loss)
+    return [total_loss, optim]
 
 # returns the interpreted data from normalized output
 def getInterpretation(output):
@@ -91,17 +111,50 @@ def getInterpretation(output):
 
     return [hour, detection, angle]
 
-def accuracy(output, truth):
-    hour, detection, angle = getInterpretation(output)
-    # in the truth daat we ignore the angle
-    true_hour, true_detection, _ = getInterpretation(truth)
+def accuracy(output_series, truth_series):
+    acc = 0
+    truths = tf.unpack(truth_series, axis=0)
+    for i in range(len(output_series)):
+        ouput = output_series[i]
+        truth = tf.reshape(truths[i],[1,3])
+    
+        hour, detection, angle = getInterpretation(output)
+        # in the truth daat we ignore the angle
+        true_hour, true_detection, _ = getInterpretation(truth)
 
-    h_correct = tf.cast(tf.equal(hour, true_hour), tf.float32)
-    d_correct = tf.cast(tf.equal(detection, true_detection), tf.float32)
+        h_correct = tf.cast(tf.equal(hour, true_hour), tf.float32)
+        d_correct = tf.cast(tf.equal(detection, true_detection), tf.float32)
 
-    h_acc = 100*tf.reduce_mean(h_correct)
-    d_acc = 100*tf.reduce_mean(d_correct)
+        h_acc = 100*tf.reduce_mean(h_correct)
+        d_acc = 100*tf.reduce_mean(d_correct)
 
-    total_acc = (h_acc + d_acc)/2
+        acc += ((h_acc + d_acc)/2)/len(output_series)
 
-    return [h_acc, d_acc, total_acc]
+    return acc
+
+# creating placeholders
+input_series = tf.placeholder(tf.float32, [TRUNC_BACKPROP_LENGTH, SENSOR_DATA_DIM])
+truth_series = tf.placeholder(tf.float32, [TRUNC_BACKPROP_LENGTH, OUTPUT_DIM])
+
+init_state = tf.placeholder(tf.float32, [SENSOR_DATA_DIM, STATE_SIZE])
+
+# creating the main model with back prop in time
+inputs = tf.unpack(input_series, axis=0)
+cur_state = init_state
+output_series = []
+for inp in inputs:
+    output, cur_state = run_model(inp, cur_state)
+    output_series.append(output)
+
+loss_tensor, optim_t = loss(output_series, truth_series)
+hour_t, detection_t, motor_t = getInterpretation(output)
+accuracy_tensor = accuracy(output_series, truth_series)
+
+# adding summaries
+with tf.name_scope('data'):
+    tf.summary.scalar('loss', loss_tensor)
+    tf.summary.scalar('hour', tf.reduce_mean(hour_t))
+    tf.summary.scalar('head_ang', tf.reduce_mean(motor_t))
+    tf.summary.scalar('accuracy', accuracy_tensor)
+
+merged = tf.summary.merge_all()
